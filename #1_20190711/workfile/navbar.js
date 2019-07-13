c6ff23f72688ed849532cdfe76e7c44031fa610f
Eojin-Lee <dgst1358@gmail.com>
2019-07-13 17:50:29 +0900

    Upload #1 20190713

--- #1_20190711/workfile/navbar.js
+++
@@ -1,7 +0,0 @@
-const item = document.querySelectorAll('.item');
-const activeBar = document.querySelector('.active-bar');
-
-function handelActiveBar(el) {
-    activeBar.style.width = `${el.offsetWidth}px`;
-    activeBar.style.left = `${el.offsetLeft}px`;
-}
